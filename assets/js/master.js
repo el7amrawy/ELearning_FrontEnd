be8623a9da8520782f5f1571d35d9ae0d1e873ce
Mohamed <123medoabdo@gmail.com>
2024-12-14 00:02:47 +0200

    alert and logo

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,18 +1,24 @@
 let menu = document.querySelector("#mobile-menu");
 let icon_menu = document.querySelector(".menu-icon")
-
 let user_menu = document.querySelector("[role=menu]")
 let user_icon = document.querySelector("#user-menu-button");
+let search_icon = document.querySelector(".search_icon");
+let search_input = document.querySelector(".search_input");
+let other_search = document.querySelector(".other-search");
 
-if(icon_menu){
-    icon_menu.addEventListener('click',function(){
+search_icon.addEventListener('click' , function(){
+    if(menu.children[0].className === "active"){
         menu.classList.toggle("hidden");
         icon_menu.childNodes[5].classList.toggle("hidden");
-        icon_menu.childNodes[7].classList.toggle("hidden")
-    })
-}
-user_icon.addEventListener('click' ,function(){
-    user_menu.classList.toggle("hidden")
+        icon_menu.childNodes[7].classList.toggle("hidden");
+        menu.children[0].classList.toggle("active");
+    }
+    if(user_icon.parentElement.parentElement.children[1].children[0].className === "active"){
+        user_icon.parentElement.parentElement.children[1].children[0].classList.toggle("active");
+        user_menu.classList.toggle("hidden");
+    }
+    search_input.classList.toggle("hidden");
+    search_input.children[0].classList.toggle("active");
 })
 
 /* ================>>> Register <<<================= */
---
+++ assets/js/sweet.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+Swal.fire({
+    title: "Welcome!",
+    text: "To Mega Courses Around World.",
+    imageUrl: "./assets/images/welcome.svg",
+    imageWidth: 300,
+    imageHeight: 200,
+    imageAlt: "Custom image",
+    confirmButtonColor: "#000",
+    confirmButtonWidth: 20,
+    confirmButtonText: "Close",
+});
+};
