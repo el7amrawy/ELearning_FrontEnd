000a32365c350862017199898468e4cfdb738b8c
Ahmed Samir <ahmed.orgline.com>
2025-06-19 15:57:59 +0300

    Add_poppup_to_homePage_and_new_navBar

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,18 +1,30 @@
-let btn_close = document.querySelector(".btn-close");
-let poppup = document.querySelector(".poppup");
+/* create poppup */
+const poppup = (content) => {
+    let tag = document.createElement("div");
+    tag.className = "text-center py-4 px-2 flex items-center justify-center text-blue-500 relative bg-blue-100 text-sm md:text-base"
+    tag.innerHTML = `
+        <p>${content}</p>
+        <span class="absolute right-5 cursor-pointer btn-close"><i class="fa-solid fa-xmark"></i></span>
+    `
+    let header = document.querySelector("header");
+    header.before(tag);
 
-/* auto hidden */
-setTimeout(() => {
-    poppup.classList.add("opacity-0");
+    /* close poppup automatice */
     setTimeout(() => {
-        poppup.classList.add("hidden");
-    } , 1000)
-}, 5000)
+        tag.classList.add("opacity-0");
+        setTimeout(() => {
+            tag.classList.add("hidden");
+        }, 1000)
+    }, 5000)
 
-/* manual hidden */
-btn_close.addEventListener('click', function () {
-    poppup.classList.add("opacity-0");
-    setTimeout(() => {
-        poppup.classList.add("hidden");
-    }, 1000)
-})
+    /* close poppup by close_btn */
+    let btn_close = document.querySelector(".btn-close");
+    btn_close.addEventListener('click', function () {
+        tag.classList.add("opacity-0");
+        setTimeout(() => {
+            tag.classList.add("hidden");
+        }, 700)
+    })
+}
+
+poppup('Ready to get with the times? Get the skills');
